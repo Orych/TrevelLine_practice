447594abcb64f68986f2d6898890af511063b9dc
Orych <49996806+Orych@users.noreply.github.com>
2022-02-17 00:55:40 +0300

    Add files via upload

---
+++ Ellipseproject/Ellipseproject/Ellipse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellipseproject
+{
+    public class Figure
+    {
+        public point Centre;
+        public int HorizontalRadius { get; set; }
+        public int VerticalRadius { get; set; }
+        public Figure
+            (
+            point Centre,
+            int Horizontalradius,
+            int Verticalradius
+            )
+        {
+            if (Horizontalradius <= 0)
+            {
+                throw new ArgumentException("Горизонтальный радиус введён неверно");
+            }
+            if (Verticalradius <= 0)
+            {
+                throw new ArgumentException("Вертикальный радиус введён неверно");
+            }
+
+            Centre = Centre;
+            HorizontalRadius = Horizontalradius;
+            VerticalRadius = Verticalradius;
+
+        }
+        public double Square()
+        {
+            return Math.Round(Math.PI * HorizontalRadius * VerticalRadius);
+        }
+        public double Lenght()
+        {
+            return Math.Round(Math.PI * (HorizontalRadius + VerticalRadius));
+        }
+    }
+}
---
+++ Ellipseproject/Ellipseproject/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ellipseproject;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        point Centre = new point
+        {
+            X = 0,
+            Y = 0
+        };
+        int HorizontalRadius = 5;
+        int VerticalRadius = 3;
+        try
+        {
+            Figure ellipse = new Figure(Centre, HorizontalRadius, VerticalRadius);
+
+            Console.WriteLine($"Площадь эллипса -> {ellipse.Square()}");
+            Console.WriteLine($"Длина дуги эллипса -> {ellipse.Lenght()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
